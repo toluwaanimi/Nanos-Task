2643e734c38c38802becab0951675b0129a8082d
toluwaanimi <emmanueltolu.adebayo@gmail.com>
2021-03-03 09:16:31 +0100

    feat(config): removed folder

--- config/env.config.js
+++
@@ -1,9 +0,0 @@
-const env = require('env-var');
-const {config} = require('dotenv')
-config()
-
-const STRIPE_KEY = env.get('STRIPE_KEY').required().asString()
-
-module.exports = {
-    STRIPE_KEY
-}
